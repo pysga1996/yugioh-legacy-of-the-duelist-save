0f9a79eeb2ef56c4bf889866cdf505b52495e51e
vutat.thanh@etc.vn <vutatthanh@96>
2021-12-09 13:28:00 +0700

    Update 09/12/2021 13:28:00,18

--- upload_save.bat
+++ upload_save.bat
@@ -1,12 +1,5 @@
 @ECHO OFF
-for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
-     set dow=%%i
-     set month=%%j
-     set day=%%k
-     set year=%%l
-)
-set datestr=%month%_%day%_%year% >con
-echo "Update %datestr%"
+echo "Update %DATE% %TIME%"
 git add .
-git commit -m "Update %datestr%"
+git commit -m "Update %DATE% %TIME%"
 git push origin main
